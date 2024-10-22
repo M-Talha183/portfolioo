a98179f808a93eb6e3d4385e68546f980180f938
Muhammad361738 <ullahehsan016@gmail.com>
2024-10-22 23:20:20 +0500

    new responsive portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ import HomePage from "./HomePage/page"
 import AboutPage from "./About/page";
 import Skills from "./Skills/page";
 import Education from "./Education/page"
+import Contact from "./Contact/page"
 
 export default function Home() {
   return (
    <>
-   <Navbar/>
+   
    <HomePage/>
    <AboutPage/>
    <Skills/>
    <Education/>
+   <Contact/>
    </>
   );
 }
