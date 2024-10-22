c53ae58f5e08771a8227b0ae61e1381dc801c752
Muhammad361738 <ullahehsan016@gmail.com>
2024-10-22 23:26:05 +0500

    new responsive portfolio

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Page = () => {
     return (
         <div>
-            
+              <div className=' w-auto h-auto md:w-ful md:h-screen bg-slate-800 items-center pb-5' >
+             <div className="h-auto w-full flex flex-wrap flex-col
+            items-center"> 
+            <p className="text-white font-bold text-3xl
+            md:text-4xl text-center mt-20">Contact Me</p>
+            <div className="w-36 h-1 border-b-4 border-yellow-400 
+            mt-2 rounded-2xl md:mt-4 mb-12">
+        </div>
+        </div>
+      </div>
         </div>
     );
 }
