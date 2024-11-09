3b9f7c8ba74f80d55fcba47efa8ec9b2aac80a85
Muhammad361738 <ullahehsan016@gmail.com>
2024-11-10 00:02:30 +0500

    some modification

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -54,8 +54,11 @@ const TestCards = () => {
     return (
         <div className="h-[40rem] w-full bg-black relative flex flex-col items-center justify-center
         overflow-hidden">
-            <h2 className="text-3xl font-bold text-center mb-8 z-10 text-white">My Projects</h2>
-            
+            <h2 className="text-3xl font-bold text-center  z-10 text-white">My Projects</h2>
+            <div className="w-36 h-1 border-b-4 border-yellow-400 
+          mt-2 rounded-2xl md:mt-4 mb-12">
+        </div>
+
                 <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8 ">
                     <div className="w-full max-w-6xl">
                           <InfiniteMovingCards
