cdeea23b0a49efb9e708847be67dddcb8098dbc0
Muhammad361738 <ullahehsan016@gmail.com>
2024-11-09 23:53:20 +0500

    some modification

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -10,10 +10,10 @@ const Page = () => {
                  {/* contact form  */}
            <div className='w-96 items-center'>      
             <form className='pl-5'>
-                <input className='p-3 w-80 md:w-96 border-black border-2 rounded-xl text-xl' type="text" name="" id="" placeholder='Name' required />
-                <input className='p-3 w-80 md:w-96 border-black border-2 rounded-xl text-xl mt-5' type="text" name="" id="" placeholder='email' required />
-                <input className='p-3 w-80 md:w-96 border-black border-2 rounded-xl text-xl mt-5' type="text" name="" id="" placeholder='phone' required />
-                <input className='p-3 w-80 md:w-96 h-32 border-black border-2 rounded-xl text-xl mt-5' type="text" name="" id="" placeholder='Message' required />
+                <input className='p-1 w-80 md:w-96 border-black border-2 rounded-xl text-xl' type="text" name="" id="" placeholder='Name' required />
+                <input className='p-1 w-80 md:w-96 border-black border-2 rounded-xl text-xl mt-5' type="text" name="" id="" placeholder='email' required />
+                <input className='p-1 w-80 md:w-96 border-black border-2 rounded-xl text-xl mt-5' type="text" name="" id="" placeholder='phone' required />
+                <input className='p-1 w-80 md:w-96 h-32 border-black border-2 rounded-xl text-xl mt-5' type="text" name="" id="" placeholder='Message' required />
 
             </form>
             </div>
