c6706a2fee6448516bfdabb3b67d67058ca9a946
Muhammad361738 <ullahehsan016@gmail.com>
2024-11-16 19:44:55 +0500

    some modification

--- src/app/HomePage/Type.tsx
+++ src/app/HomePage/Type.tsx
@@ -6,7 +6,7 @@ export default function TypewriterEffect() {
       <span className="text-3xl sm:3xl lg:text-3xl text-amber-600 font-bold">
         {/* Typewriter component usage */}
         <Typewriter
-          words={[ 'Frontend Developer', 'UI/UX Designer','Figma Designer','Learning AI']}
+          words={[ 'Frontend Developer', 'UI/UX Designer','Figma Designer','Coder','']}
           loop={false} // Number of times to loop; set `true` for infinite
           cursor
           cursorStyle="|" // Custom cursor style
