b917af19d4a62cd96bfa57b244e0f880b93e723f
Muhammad361738 <ullahehsan016@gmail.com>
2025-01-27 21:54:16 +0500

    Add new project in project section

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -19,6 +19,13 @@ const quotesArray = [
       
     },
     {
+      quote: "Coffie App",
+    image : coffee,
+    link : "https://coffe-website-21.vercel.app/",
+    
+    
+  },
+    {
         quote: "Restaurent App",
 
       image : rest,
